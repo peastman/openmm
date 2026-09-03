b399af4725573963b46d6c1083fdcf7a37615857
Peter Eastman <peastman@stanford.edu>
2026-09-03 16:32:54 -0700

    Changes for 8.6.1 (#5414)
    * Removed debugging code (#5396)
    
    * Fix a potential segfault in PythonForce (#5400)
    
    * DPDIntegrator works correctly with non-consecutive particle types (#5401)
    
    * Fix error updating tabulated function for CustomNonbondedForce (#5403)
    
    * Fix atom ordering in QTBIntegrator (#5408)
    
    * Clone CustomNonbondedForce with XmlSerializer::clone() (#5411)
    
    * Disable worker thread in PythonForce (#5412)
    
    * Update version number to 8.6.1

--- platforms/common/src/CommonKernels.cpp
+++ platforms/common/src/CommonKernels.cpp
@@ -3644,26 +3644,17 @@ void CommonIntegrateDPDStepKernel::execute(ContextImpl& context, const DPDIntegr
 
     // Perform the integration.
 
-    vector<int> p;
-    particleType.download(p);
     double stepSize = integrator.getStepSize();
     cc.getIntegrationUtilities().setNextStepSize(stepSize);
     kernel1->execute(numAtoms);
-    particleType.download(p);
     integration.applyVelocityConstraints(integrator.getConstraintTolerance());
-    particleType.download(p);
     kernel2->setArg(9, randomSeed+cc.getStepCount());
     kernel2->setArg(10, (float) (BOLTZ*integrator.getTemperature()));
     setPeriodicBoxArgs(cc, kernel2, 11);
-    particleType.download(p);
     kernel2->execute(2*nb.getNumForceThreadBlocks()*blockSize, blockSize);
-    particleType.download(p);
     kernel3->execute(numAtoms);
-    particleType.download(p);
     integration.applyConstraints(integrator.getConstraintTolerance());
-    particleType.download(p);
     kernel4->execute(numAtoms);
-    particleType.download(p);
     integration.computeVirtualSites();
 
     // Update the time and step count.
@@ -3675,7 +3666,6 @@ void CommonIntegrateDPDStepKernel::execute(ContextImpl& context, const DPDIntegr
     // Reduce UI lag.
 
     flushPeriodically(cc);
-    particleType.download(p);
 }
 
 double CommonIntegrateDPDStepKernel::computeKineticEnergy(ContextImpl& context, const DPDIntegrator& integrator) {
@@ -4877,7 +4867,9 @@ void CommonCalcPythonForceKernel::initialize(const ContextImpl& context, const P
         addForcesKernel->addArg(cc.getAtomIndexArray());
     }
     forceGroupFlag = (1<<force.getForceGroup());
-    useWorkerThread = (cc.getNumContexts() == 1);
+    // Disable the worker thread until we figure out what is causing incompatibilities with PyTorch.
+//    useWorkerThread = (cc.getNumContexts() == 1);
+    useWorkerThread = false;
     for (const ForceImpl* impl : context.getForceImpls())
         if (&impl->getOwner() != &force && (dynamic_cast<const CustomCPPForceImpl*>(impl) != NULL || dynamic_cast<const PythonForceImpl*>(impl) != NULL))
             useWorkerThread = false;
